68538dcb91e3ec949a41fd626083a2fe6b7f326e
LaFouine25 <lafouine.becs@gmail.com>
2018-09-14 18:41:20 +0200

    Bug real_escape

--- register.php
+++ register.php
@@ -24,13 +24,13 @@ require_once('includes/dbconnect.php');
 			<div id="wrapper">
 				<div id="content">
 					<div class="innermargin">
-						<h1>Login - <?php printf(SITENAME);?></h1>
+						<h1>Enregistrement - <?php printf(SITENAME);?></h1>
 						<br />
 						<form method=POST>
 							Login: <input type="text" name="amuser" size="20" required><br />
 							MDP: <input type="password" name="ampass" size="20" required><br />
 							Email: <input type="text" name="ammail" size="20" required><br />
-							<input type="submit" value="submit" /><br /><br />
+							<input type="submit" value="Créer le Wallet" /><br /><br />
 							<?php
 if ( isset($_POST['amuser']) && isset($_POST['ampass']) && isset($_POST['ammail']) )
 {
@@ -38,7 +38,7 @@ if ( isset($_POST['amuser']) && isset($_POST['ampass']) && isset($_POST['ammail'
 	if ($DBIsCo)
 	{
 		if(DEBUG) { printf("DEBUG: Co OK<br />\r\n"); }
-		$DBReq	= 'SELECT 1 FROM comptes WHERE login LIKE "' . mysql_real_escape_string($_POST['amuser']) . '" OR email LIKE "' . mysql_real_escape_string($_POST['ammail']) . '";';
+		$DBReq	= 'SELECT 1 FROM comptes WHERE login LIKE "' . mysqli_real_escape_string($_POST['amuser']) . '" OR email LIKE "' . mysqli_real_escape_string()$_POST['ammail']) . '";';
 		$rs = $conn->query($DBReq);
  
 		if($rs === false)
@@ -48,9 +48,9 @@ if ( isset($_POST['amuser']) && isset($_POST['ampass']) && isset($_POST['ammail'
 			
 			// Définition variables
 			$curaddress = $bitcoin->getaccountaddress($_POST['amuser']);
-			$login 		= mysql_real_escape_string($_POST['amuser']);
-			$mdp		= mysql_real_escape_string($_POST['ampass']);
-			$email		= mysql_real_escape_string($_POST['ammail']);
+			$login 		= mysqli_real_escape_string($_POST['amuser']);
+			$mdp		= mysqli_real_escape_string($_POST['ampass']);
+			$email		= mysqli_real_escape_string($_POST['ammail']);
 			
 			// Requete
 			$DBReq = "INSERT INTO comptes VALUES ('$curaddress', 'RVN', '$login', '$mdp', '0', '$email');";
@@ -74,11 +74,6 @@ if ( isset($_POST['amuser']) && isset($_POST['ampass']) && isset($_POST['ammail'
 }
 ?>
 						</table></form>
-						<br />
-						<a href="register.php">Créer un compte gratuit</a>
-						<br />
-						Merci d'utiliser notre Wallet Online. Celui-ci vous permet de sécuriser vos Crypto Coins sans avoir à télécharger l'entier de la BlockChain.<br />
-						Vous pouvez également effectuer des transactions vers d'autres Wallet d'un simple clic.<br />
 						Ce Wallet exploite le Pool de minage : <a href=http://<?php echo SERVER;?> target=blank><?php echo SITENAME;?></a><br />
 					</div>
 				</div>
